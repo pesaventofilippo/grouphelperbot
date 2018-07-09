6fdfe34779db5371ebe57bd8f015c808e49fdfac
Filippo Pesavento <pesaventofilippo@gmail.com>
2018-07-09 13:40:06 +0200

    Exported settings in external file

--- bot.py
+++ bot.py
@@ -1,45 +1,12 @@
 ﻿import telepot, time
 from tinydb import TinyDB, where
-db_users = TinyDB('users.json')
-db_admins = TinyDB('admins.json')
-
-
-def initialize():
-    try:
-        f = open("bot_token.txt", "r")
-        token = str(f.readline())
-        bot = telepot.Bot(token)
-        f.close()
-    except FileNotFoundError:
-        f = open("bot_token.txt", "w")
-        token = str(input("Token File not found. Please insert the HTTP API Bot Token: "))
-        bot = telepot.Bot(token)
-        f.write(token)
-        f.close()
-    try:
-        f = open("group_id.txt", "r")
-        group = int(f.readline())
-        f.close()
-    except FileNotFoundError:
-        f = open("group_id.txt", "w")
-        group = int(input("Group ID File not found. Please insert the group Chat ID: "))
-        f.write(str(group))
-        f.close()
-
-    groupUserCount = bot.getChatMembersCount(group)
-    myusername = "@" + bot.getMe()['username']
-    print("Bot started...")
-    return bot, group, groupUserCount, myusername
-
-
-def reloadAdmins():
-    for x in bot.getChatAdministrators(group):
-        if x['user']['is_bot']:
-            updateAdminDatabase(x['user']['id'], "bot")
-        elif x['status'] == "administrator":
-            updateAdminDatabase(x['user']['id'], "admin")
-        elif x['status'] == "creator":
-            updateAdminDatabase(x['user']['id'], "creator")
+import settings
+db_users = TinyDB(settings.Databases.users)
+db_admins = TinyDB(settings.Databases.admins)
+bot = telepot.Bot(settings.Bot.token)
+group = settings.Bot.groupId
+groupUserCount = bot.getChatMembersCount(group)
+myusername = "@" + bot.getMe()['username']
 
 
 def updateAdminDatabase(id, status):
@@ -56,43 +23,52 @@ def updateUserDatabase(id, firstName, lastName, username):
         db_users.insert({'chatId': id, 'firstName': firstName, 'lastName': lastName, 'username': username, 'warns': "0"})
 
 
+def reloadAdmins():
+    for x in bot.getChatAdministrators(group):
+        if x['user']['is_bot']:
+            updateAdminDatabase(x['user']['id'], "bot")
+        elif x['status'] == "administrator":
+            updateAdminDatabase(x['user']['id'], "admin")
+        elif x['status'] == "creator":
+            updateAdminDatabase(x['user']['id'], "creator")
+
+
 def getUserInfo(msg):
-    chatId = int(msg['chat']['id'])
-    msgId = int(msg['message_id'])
-    from_id = int(msg['from']['id'])
+    chatId = msg['chat']['id']
+    msgId = msg['message_id']
+    from_id = msg['from']['id']
 
     try:
-        text = str(msg['text'])
+        text = msg['text']
     except KeyError:
         text = ""
     try:
-        from_firstName = str(msg['from']['first_name'])
+        from_firstName = msg['from']['first_name']
     except KeyError:
         from_firstName = ""
     try:
-        from_lastName = str(msg['from']['last_name'])
+        from_lastName = msg['from']['last_name']
     except KeyError:
         from_lastName = ""
     try:
-        from_username = str(msg['from']['username'])
+        from_username = msg['from']['username']
     except KeyError:
         from_username = ""
 
     try:
-        x = msg['reply_to_message']
         isReply = True
-        reply_msgId = int(msg['reply_to_message']['message_id'])
-        reply_fromId = int(msg['reply_to_message']['from']['id'])
+        reply_msgId = msg['reply_to_message']['message_id']
+        reply_fromId = msg['reply_to_message']['from']['id']
         try:
-            reply_firstName = str(msg['reply_to_message']['from']['first_name'])
+            reply_firstName = msg['reply_to_message']['from']['first_name']
         except KeyError:
             reply_firstName = ""
         try:
-            reply_lastName = str(msg['reply_to_message']['from']['last_name'])
+            reply_lastName = msg['reply_to_message']['from']['last_name']
         except KeyError:
             reply_lastName = ""
         try:
-            reply_username = str(msg['reply_to_message']['from']['username'])
+            reply_username = msg['reply_to_message']['from']['username']
         except KeyError:
             reply_username = ""
 
@@ -103,7 +79,6 @@ def getUserInfo(msg):
         reply_firstName = None
         reply_lastName = None
         reply_username = None
-
 
     return chatId, msgId, text, from_id, from_firstName, from_lastName, from_username, isReply, reply_msgId, reply_fromId, reply_firstName, reply_lastName, reply_username
 
@@ -128,7 +103,13 @@ def handle(msg):
 
         # Welcoming message
         if bot.getChatMembersCount(group) > groupUserCount:
-            bot.sendMessage(group, "Hi, <b>"+from_firstName+"</b>!\nWelcome in the "+bot.getChat(group)['title']+" group!", "HTML")
+            data = settings.Messages.welcome
+            data = data.replace('{{name}}', from_firstName)
+            data = data.replace('{{surname}}', from_lastName)
+            data = data.replace('{{username}}', from_username)
+            data = data.replace('{{id}}', from_id)
+            data = data.replace('{{group_name}}', bot.getChat(group)['title'])
+            bot.sendMessage(group, data, "HTML")
 
         groupUserCount = bot.getChatMembersCount(group)
 
@@ -244,10 +225,10 @@ def handle(msg):
                 userWarns = int(db_users.search(where('chatId') == selectedUserData)[0]['warns'])
                 try:
                     reason = text_split[2]
-                    bot.sendMessage(group, str("❗️️ " + selectedUser + " has been warned [" + str(userWarns) + "/3] for <b>" + reason + "</b>."), parse_mode="HTML")
+                    bot.sendMessage(group, str("❗️️ " + selectedUser + " has been warned [" + str(userWarns) + "/" + str(settings.Moderation.maxWarns)+"] for <b>" + reason + "</b>."), parse_mode="HTML")
                 except IndexError:
-                    bot.sendMessage(group, str("❗️️ " + selectedUser + " has been warned [" + str(userWarns) + "/3]."))
-                if userWarns >= 3:
+                    bot.sendMessage(group, str("❗️️ " + selectedUser + " has been warned [" + str(userWarns) + "/" + str(settings.Moderation.maxWarns)+"]."))
+                if userWarns >= settings.Moderation.maxWarns:
                     bot.kickChatMember(group, selectedUserData)
                     db_users.update({'warns': "0"}, where('chatId') == selectedUserData)
                     bot.sendMessage(group, str("🔇️ " + selectedUser + " has been banned."))
@@ -262,10 +243,10 @@ def handle(msg):
                 userWarns = int(db_users.search(where('chatId') == selectedUserData)[0]['warns'])
                 try:
                     reason = text_split[2]
-                    bot.sendMessage(group, str("❗️️ " + selectedUser + " has been warned [" + str(userWarns) + "/3] for <b>" + reason + "</b>."), parse_mode="HTML")
+                    bot.sendMessage(group, str("❗️️ " + selectedUser + " has been warned [" + str(userWarns) + "/" + str(settings.Moderation.maxWarns)+"] for <b>" + reason + "</b>."), parse_mode="HTML")
                 except IndexError:
-                    bot.sendMessage(group, str("❗️️ " + selectedUser + " has been warned [" + str(userWarns) + "/3]."))
-                if userWarns >= 3:
+                    bot.sendMessage(group, str("❗️️ " + selectedUser + " has been warned [" + str(userWarns) + "/" + str(settings.Moderation.maxWarns)+"]."))
+                if userWarns >= settings.Moderation.maxWarns:
                     bot.kickChatMember(group, selectedUserData)
                     db_users.update({'warns': "0"}, where('chatId') == selectedUserData)
                     bot.sendMessage(group, str("🔇️ " + selectedUser + " has been banned."))
@@ -335,25 +316,25 @@ def handle(msg):
                     userWarns = int(db_users.search(where('chatId') == reply_fromId)[0]['warns'])
                     try:
                         reason = text.split(" ", 1)[1]
-                        bot.sendMessage(group, str("❗️️ " + reply_firstName + " has been warned [" + str(userWarns) + "/3] for <b>" + reason + "</b>."), parse_mode="HTML", reply_to_message_id=reply_msgId)
+                        bot.sendMessage(group, str("❗️️ " + reply_firstName + " has been warned [" + str(userWarns) + "/" + str(settings.Moderation.maxWarns) + "] for <b>" + reason + "</b>."), parse_mode="HTML", reply_to_message_id=reply_msgId)
                     except IndexError:
-                        bot.sendMessage(group, str("❗️️ " + reply_firstName + " has been warned [" + str(userWarns) + "/3]."), reply_to_message_id=reply_msgId)
-                    if userWarns >= 3:
+                        bot.sendMessage(group, str("❗️️ " + reply_firstName + " has been warned [" + str(userWarns) + "/" + str(settings.Moderation.maxWarns) + "]."), reply_to_message_id=reply_msgId)
+                    if userWarns >= settings.Moderation.maxWarns:
                         bot.kickChatMember(group, reply_fromId)
                         db_users.update({'warns': "0"}, where('chatId') == reply_fromId)
                         bot.sendMessage(group, str("🔇️ " + reply_firstName + " has been banned."), reply_to_message_id=reply_msgId)
 
-                if text.startswith("/delwarn"):
+                elif text.startswith("/delwarn"):
                     bot.deleteMessage((group, reply_msgId))
                     previousWarns = int(db_users.search(where('chatId') == reply_fromId)[0]['warns'])
                     db_users.update({'warns': str(previousWarns + 1)}, where('chatId') == reply_fromId)
                     userWarns = int(db_users.search(where('chatId') == reply_fromId)[0]['warns'])
                     try:
                         reason = text.split(" ", 1)[1]
-                        bot.sendMessage(group, str("❗️️ " + reply_firstName + " has been warned [" + str(userWarns) + "/3] for <b>" + reason + "</b>."), parse_mode="HTML")
+                        bot.sendMessage(group, str("❗️️ " + reply_firstName + " has been warned [" + str(userWarns) + "/" + str(settings.Moderation.maxWarns) + "] for <b>" + reason + "</b>."), parse_mode="HTML")
                     except IndexError:
-                        bot.sendMessage(group, str("❗️️ " + reply_firstName + " has been warned [" + str(userWarns) + "/3]."))
-                    if userWarns >= 3:
+                        bot.sendMessage(group, str("❗️️ " + reply_firstName + " has been warned [" + str(userWarns) + "/" + str(settings.Moderation.maxWarns) + "]."))
+                    if userWarns >= settings.Moderation.maxWarns:
                         bot.kickChatMember(group, reply_fromId)
                         db_users.update({'warns': "0"}, where('chatId') == reply_fromId)
                         bot.sendMessage(group, str("🔇️ " + reply_firstName + " has been banned."))
@@ -451,7 +432,7 @@ def handle(msg):
             bot.sendMessage(group, message, parse_mode="HTML")
 
 
-bot, group, groupUserCount, myusername = initialize()
+print("Bot started...")
 reloadAdmins()
 bot.message_loop({'chat': handle})
 while True:
---
+++ settings-example.py
@@ -0,0 +1,16 @@
+class Bot:
+    token = '123456789:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    groupId = -1001234567890
+
+
+class Databases:
+    admins = 'admins.json'
+    users = 'users.json'
+
+
+class Messages:
+    welcome = "Hi, <b>{{name}}</b>!\nWelcome in the {{group_name}} group!"
+
+
+class Moderation:
+    maxWarns = 3
