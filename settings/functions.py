34f9987b75b16ddcc057aa429bd8f89a62c17ffa
pesaventofilippo <pesaventofilippo@gmail.com>
2018-07-27 16:05:14 +0200

    Fixed language import

--- settings/functions.py
+++ settings/functions.py
@@ -2,7 +2,10 @@ import settings
 
 def getStr(string, args=None):
     import importlib
-    locale = importlib.import_module("settings.lang_"+Bot.language)
+    try:
+        locale = importlib.import_module("settings.lang_"+settings.settings.Bot.language)
+    except ImportError:
+        locale = settings.lang_en
     if string in locale.dict:
         if args == None:
             return locale.dict[string]
